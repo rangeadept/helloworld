f5b75b446cfe8e56557dc4843b00289225c70343
Range Strunk <range@adeptable.com>
2016-06-29 15:24:47 -0700

    Moving scripts to separate repo, other cleanups

--- build/find-healthy-pods.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-kubectl get pods --selector=name=$APP_NAME \
-            -o=jsonpath='{range .items[*]}{@.metadata.name}:{range @.status.conditions[*]}{@.type}={@.status};{end}{end}'| \
-             tr ';' "\n" |  grep "Ready=True" 
